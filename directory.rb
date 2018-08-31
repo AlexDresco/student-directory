bd0990eb298cb0d8d28e1c7407cdcc99d2329b28
Alexandra Dresco <alexandra.d1510@gmail.com>
2018-08-31 13:36:00 +0100

    directory interactive

--- directory.rb
+++ directory.rb
@@ -47,8 +47,26 @@ def print_footer(names)
   end
 end
 
-#call the methods to make things happen
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Enter students' details"
+    puts "2. Show the students"
+    puts "9. Exit"
+  selection = gets.chomp
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
