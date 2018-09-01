70be3e889f23de20ce6fa1ef323698a5d5546dbf
Alexandra Dresco <alexandra.d1510@gmail.com>
2018-09-01 17:04:38 +0100

    Loads students.csv automatically if no file is provided

--- directory.rb
+++ directory.rb
@@ -92,6 +92,8 @@ end
 def try_load_students
   filename = ARGV.first
   return if filename.nil?
+    filename = "students.csv"
+  end
   if File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
