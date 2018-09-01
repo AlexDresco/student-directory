7d19ea78bd210a7169533269c4e2da23bacdc28e
Alexandra Dresco <alexandra.d1510@gmail.com>
2018-09-01 16:29:25 +0100

    Students list final

--- directory.rb
+++ directory.rb
@@ -1,37 +1,5 @@
 @students = []
 
-def input_students
-  puts "Please enter your name and cohort"
-  puts "To finish, just hit return twice when prompted for name"
-  puts "Name:"
-  name = gets.chomp
-
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-end
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  if @students.count == 1
-    puts "Overall, we have #{@students.count} great student"
-  else
-  puts "Overall, we have #{@students.count} great students"
-  end
-end
-
 def print_menu
   puts "1. Enter students' details"
   puts "2. Show the students"
@@ -40,10 +8,11 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -63,25 +32,51 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+def input_students
+  puts "Please enter the students' names"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
   end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def save_students
   file = File.open("students.csv","w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join
+    csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort}
@@ -89,4 +84,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
